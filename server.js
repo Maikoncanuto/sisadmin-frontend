7fb15d7fb59a3c4cbcca7077321d557e09111ac1
Maikon Canuto <maikoncanuto@gmail.com>
2019-01-08 22:42:42 -0200

    Configuração para o herooku

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express ();
 
 // Serve apenas os arquivos estáticos que formam o diretório dist
 console.log('Passo 1 - express.static: /dist/');
-app.use (express.static (__dirname + '/dist/'));
+app.use (express.static (__dirname + '/dist/app-sisadmin'));
 
 console.log('Passo 1 - app.get: /*');
 app.get ('/*', function (req, res) {
-  res.sendFile (path.join (__dirname + '/dist/index.html'));
+  res.sendFile (path.join (__dirname + '/dist/app-sisadmin/index.html'));
 });
 
 // Inicie o aplicativo ouvindo na porta padrão do Heroku
